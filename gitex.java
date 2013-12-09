2dc9be20382a431b18f6d1f28c82fae5345c8344
kasunsk <kasunsk@gmail.com>
2013-12-09 11:03:34 +0530

    next commit

---
+++ gitex.java
@@ -0,0 +1,13 @@
+public class gitex {
+	public static void main(String [] args){
+		sayHello();
+	}
+
+	public static void sayHello(){
+		System.out.println("Hello Friend");
+	}
+
+	public static void sayBye(){
+		System.out.println("Bye Friend");
+	}
+}
